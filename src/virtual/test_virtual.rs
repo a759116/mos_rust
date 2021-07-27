12d9c2fa6ed28173a79d173f7a3b0f72b0064609
Rudra Panda <rudra.panda@target.com>
2021-07-26 20:51:35 -0500

    virtual complete with all test cases

--- src/virtual/test_virtual.rs
+++ src/virtual/test_virtual.rs
@@ -1,7 +1,10 @@
 #[cfg(test)]
 
 mod test_virtual {
-    use crate::r#virtual::r#virtual::{process_page_access_fifo, PTE};
+    use crate::r#virtual::r#virtual::{
+        count_page_faults_fifo, count_page_faults_lru, process_page_access_fifo,
+        process_page_access_lru, PTE, process_page_access_lfu, count_page_faults_lfu,
+    };
 
     fn set_pte(
         is_valid: bool,
@@ -58,7 +61,7 @@ mod test_virtual {
     }
 
     #[test]
-    pub fn test_process_page_access_fifo_page_in_memory () {
+    pub fn test_process_page_access_fifo_page_in_memory() {
         let mut page_table: Vec<PTE> = Vec::new();
         let mut frame_pool: Vec<i32> = Vec::new();
 
@@ -83,7 +86,7 @@ mod test_virtual {
         for _i in 0..8 {
             expected_page_table.push(PTE::default());
         }
-        
+
         expected_page_table[2] = set_pte(true, 10, 3, 14, 2);
         expected_page_table[5] = set_pte(true, 20, 2, 4, 2);
         expected_page_table[7] = set_pte(true, 30, 1, 1, 1);
@@ -98,7 +101,7 @@ mod test_virtual {
     }
 
     #[test]
-    pub fn test_process_page_access_fifo_page_not_in_memory_frame_available () {
+    pub fn test_process_page_access_fifo_page_not_in_memory_frame_available() {
         let mut page_table: Vec<PTE> = Vec::new();
         let mut frame_pool: Vec<i32> = Vec::new();
 
@@ -108,7 +111,7 @@ mod test_virtual {
 
         page_table[5] = set_pte(true, 20, 2, 4, 2);
         page_table[7] = set_pte(true, 30, 1, 1, 1);
-        
+
         let page_number: usize = 3;
         frame_pool.push(10);
         let current_timestamp: i32 = 15;
@@ -124,7 +127,7 @@ mod test_virtual {
         for _i in 0..8 {
             expected_page_table.push(PTE::default());
         }
-        
+
         expected_page_table[3] = set_pte(true, 10, 15, 15, 1);
         expected_page_table[5] = set_pte(true, 20, 2, 4, 2);
         expected_page_table[7] = set_pte(true, 30, 1, 1, 1);
@@ -139,7 +142,7 @@ mod test_virtual {
     }
 
     #[test]
-    pub fn test_process_page_access_fifo_page_not_in_memory_no_frame_available () {
+    pub fn test_process_page_access_fifo_page_not_in_memory_no_frame_available() {
         let mut page_table: Vec<PTE> = Vec::new();
         let mut frame_pool: Vec<i32> = Vec::new();
 
@@ -150,7 +153,7 @@ mod test_virtual {
         page_table[3] = set_pte(true, 10, 15, 15, 1);
         page_table[5] = set_pte(true, 20, 12, 16, 2);
         page_table[7] = set_pte(true, 30, 14, 14, 1);
-        
+
         let page_number: usize = 2;
         let current_timestamp: i32 = 17;
 
@@ -165,7 +168,7 @@ mod test_virtual {
         for _i in 0..8 {
             expected_page_table.push(PTE::default());
         }
-        
+
         expected_page_table[2] = set_pte(true, 20, 17, 17, 1);
         expected_page_table[3] = set_pte(true, 10, 15, 15, 1);
         expected_page_table[7] = set_pte(true, 30, 14, 14, 1);
@@ -178,4 +181,429 @@ mod test_virtual {
         assert_eq!(frame_pool.len(), 0);
         assert_eq!(frame, 20);
     }
+
+    #[test]
+    pub fn test_count_page_faults_fifo() {
+        let mut page_table: Vec<PTE> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        let reference_string = vec![0, 3, 2, 6, 3, 4, 5, 2, 4, 5, 6];
+        let mut frame_pool: Vec<i32> = vec![0, 1, 2];
+
+        let faults = count_page_faults_fifo(&mut page_table, reference_string, &mut frame_pool);
+
+        assert_eq!(faults, 8);
+    }
+
+    #[test]
+    pub fn test_count_page_faults_fifo_case2() {
+        let mut page_table: Vec<PTE> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        page_table[2] = set_pte(true, 10, 3, 3, 1);
+        page_table[5] = set_pte(true, 20, 2, 4, 2);
+        page_table[7] = set_pte(true, 30, 1, 1, 1);
+
+        let reference_string = vec![2, 5, 0, 7, 4, 2, 3, 5, 1, 2, 6, 0];
+        let mut frame_pool: Vec<i32> = Vec::new();
+
+        let faults = count_page_faults_fifo(&mut page_table, reference_string, &mut frame_pool);
+
+        assert_eq!(faults, 10);
+    }
+
+    #[test]
+    pub fn test_process_page_access_lru() {
+        let mut page_table: Vec<PTE> = Vec::new();
+        let mut frame_pool: Vec<i32> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        page_table[2] = set_pte(true, 10, 3, 3, 1);
+        page_table[5] = set_pte(true, 20, 2, 4, 2);
+        page_table[7] = set_pte(true, 30, 1, 1, 1);
+        let page_number: usize = 0;
+        let current_timestamp: i32 = 12;
+
+        let frame = process_page_access_lru(
+            &mut page_table,
+            page_number,
+            &mut frame_pool,
+            current_timestamp,
+        );
+
+        let mut expected_page_table: Vec<PTE> = Vec::new();
+        for _i in 0..8 {
+            expected_page_table.push(PTE::default());
+        }
+        expected_page_table[0] = set_pte(true, 30, 12, 12, 1);
+        expected_page_table[2] = set_pte(true, 10, 3, 3, 1);
+        expected_page_table[5] = set_pte(true, 20, 2, 4, 2);
+        for i in 0..8 {
+            assert_eq!(page_table[i], expected_page_table[i]);
+        }
+        assert_eq!(page_table.len(), 8);
+        assert_eq!(frame, 30);
+    }
+
+    #[test]
+    pub fn test_process_page_access_lru_page_in_memory() {
+        let mut page_table: Vec<PTE> = Vec::new();
+        let mut frame_pool: Vec<i32> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        page_table[2] = set_pte(true, 10, 3, 3, 1);
+        page_table[5] = set_pte(true, 20, 2, 4, 2);
+        page_table[7] = set_pte(true, 30, 1, 1, 1);
+        let page_number: usize = 2;
+        let current_timestamp: i32 = 14;
+
+        let frame = process_page_access_lru(
+            &mut page_table,
+            page_number,
+            &mut frame_pool,
+            current_timestamp,
+        );
+
+        let mut expected_page_table: Vec<PTE> = Vec::new();
+        for _i in 0..8 {
+            expected_page_table.push(PTE::default());
+        }
+
+        expected_page_table[2] = set_pte(true, 10, 3, 14, 2);
+        expected_page_table[5] = set_pte(true, 20, 2, 4, 2);
+        expected_page_table[7] = set_pte(true, 30, 1, 1, 1);
+
+        for i in 0..8 {
+            assert_eq!(page_table[i], expected_page_table[i]);
+        }
+
+        assert_eq!(page_table.len(), 8);
+        assert_eq!(frame_pool.len(), 0);
+        assert_eq!(frame, 10);
+    }
+
+    #[test]
+    pub fn test_process_page_access_lru_page_not_in_memory_frame_available() {
+        let mut page_table: Vec<PTE> = Vec::new();
+        let mut frame_pool: Vec<i32> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        page_table[5] = set_pte(true, 20, 2, 4, 2);
+        page_table[7] = set_pte(true, 30, 1, 1, 1);
+
+        let page_number: usize = 3;
+        frame_pool.push(10);
+        let current_timestamp: i32 = 15;
+
+        let frame = process_page_access_lru(
+            &mut page_table,
+            page_number,
+            &mut frame_pool,
+            current_timestamp,
+        );
+
+        let mut expected_page_table: Vec<PTE> = Vec::new();
+        for _i in 0..8 {
+            expected_page_table.push(PTE::default());
+        }
+
+        expected_page_table[3] = set_pte(true, 10, 15, 15, 1);
+        expected_page_table[5] = set_pte(true, 20, 2, 4, 2);
+        expected_page_table[7] = set_pte(true, 30, 1, 1, 1);
+
+        for i in 0..8 {
+            assert_eq!(page_table[i], expected_page_table[i]);
+        }
+
+        assert_eq!(page_table.len(), 8);
+        assert_eq!(frame_pool.len(), 0);
+        assert_eq!(frame, 10);
+    }
+
+    #[test]
+    pub fn test_process_page_access_lru_page_not_in_memory_no_frame_available() {
+        let mut page_table: Vec<PTE> = Vec::new();
+        let mut frame_pool: Vec<i32> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        page_table[3] = set_pte(true, 10, 15, 15, 1);
+        page_table[5] = set_pte(true, 20, 12, 16, 2);
+        page_table[7] = set_pte(true, 30, 14, 14, 1);
+
+        let page_number: usize = 2;
+        let current_timestamp: i32 = 17;
+
+        let frame = process_page_access_lru(
+            &mut page_table,
+            page_number,
+            &mut frame_pool,
+            current_timestamp,
+        );
+
+        let mut expected_page_table: Vec<PTE> = Vec::new();
+        for _i in 0..8 {
+            expected_page_table.push(PTE::default());
+        }
+
+        expected_page_table[2] = set_pte(true, 30, 17, 17, 1);
+        expected_page_table[3] = set_pte(true, 10, 15, 15, 1);
+        expected_page_table[5] = set_pte(true, 20, 12, 16, 2);
+
+        for i in 0..8 {
+            assert_eq!(page_table[i], expected_page_table[i]);
+        }
+
+        assert_eq!(page_table.len(), 8);
+        assert_eq!(frame_pool.len(), 0);
+        assert_eq!(frame, 30);
+    }
+
+    #[test]
+    pub fn test_count_page_faults_lru() {
+        let mut page_table: Vec<PTE> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        let reference_string = vec![0, 3, 2, 6, 3, 4, 5, 2, 4, 6, 5];
+        let mut frame_pool: Vec<i32> = vec![0, 1, 2];
+
+        let faults = count_page_faults_lru(&mut page_table, reference_string, &mut frame_pool);
+
+        assert_eq!(faults, 9);
+    }
+
+    #[test]
+    pub fn test_count_page_faults_lru_case2() {
+        let mut page_table: Vec<PTE> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        page_table[2] = set_pte(true, 10, 3, 3, 1);
+        page_table[5] = set_pte(true, 20, 2, 4, 2);
+        page_table[7] = set_pte(true, 30, 1, 1, 1);
+
+        let reference_string = vec![2,5,0,2,5,2,3,5,1,2,6,0];
+        let mut frame_pool: Vec<i32> = Vec::new();
+
+        let faults = count_page_faults_lru(&mut page_table, reference_string, &mut frame_pool);
+
+        assert_eq!(faults, 6);
+    }
+
+    #[test]
+    pub fn test_process_page_access_lfu() {
+        let mut page_table: Vec<PTE> = Vec::new();
+        let mut frame_pool: Vec<i32> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        page_table[2] = set_pte(true, 10, 3, 3, 1);
+        page_table[5] = set_pte(true, 20, 2, 4, 2);
+        page_table[7] = set_pte(true, 30, 1, 1, 1);
+        let page_number: usize = 0;
+        let current_timestamp: i32 = 12;
+
+        let frame = process_page_access_lfu(
+            &mut page_table,
+            page_number,
+            &mut frame_pool,
+            current_timestamp,
+        );
+
+        let mut expected_page_table: Vec<PTE> = Vec::new();
+        for _i in 0..8 {
+            expected_page_table.push(PTE::default());
+        }
+        expected_page_table[0] = set_pte(true, 30, 12, 12, 1);
+        expected_page_table[2] = set_pte(true, 10, 3, 3, 1);
+        expected_page_table[5] = set_pte(true, 20, 2, 4, 2);
+        for i in 0..8 {
+            assert_eq!(page_table[i], expected_page_table[i]);
+        }
+        assert_eq!(page_table.len(), 8);
+        assert_eq!(frame, 30);
+    }
+
+    #[test]
+    pub fn test_process_page_access_lfu_page_in_memory() {
+        let mut page_table: Vec<PTE> = Vec::new();
+        let mut frame_pool: Vec<i32> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        page_table[2] = set_pte(true, 10, 3, 3, 1);
+        page_table[5] = set_pte(true, 20, 2, 4, 2);
+        page_table[7] = set_pte(true, 30, 1, 1, 1);
+        let page_number: usize = 2;
+        let current_timestamp: i32 = 14;
+
+        let frame = process_page_access_lfu(
+            &mut page_table,
+            page_number,
+            &mut frame_pool,
+            current_timestamp,
+        );
+
+        let mut expected_page_table: Vec<PTE> = Vec::new();
+        for _i in 0..8 {
+            expected_page_table.push(PTE::default());
+        }
+
+        expected_page_table[2] = set_pte(true, 10, 3, 14, 2);
+        expected_page_table[5] = set_pte(true, 20, 2, 4, 2);
+        expected_page_table[7] = set_pte(true, 30, 1, 1, 1);
+
+        for i in 0..8 {
+            assert_eq!(page_table[i], expected_page_table[i]);
+        }
+
+        assert_eq!(page_table.len(), 8);
+        assert_eq!(frame_pool.len(), 0);
+        assert_eq!(frame, 10);
+    }
+
+    #[test]
+    pub fn test_process_page_access_lfu_page_not_in_memory_frame_available() {
+        let mut page_table: Vec<PTE> = Vec::new();
+        let mut frame_pool: Vec<i32> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        page_table[5] = set_pte(true, 20, 2, 4, 2);
+        page_table[7] = set_pte(true, 30, 1, 1, 1);
+
+        let page_number: usize = 3;
+        frame_pool.push(10);
+        let current_timestamp: i32 = 15;
+
+        let frame = process_page_access_lfu(
+            &mut page_table,
+            page_number,
+            &mut frame_pool,
+            current_timestamp,
+        );
+
+        let mut expected_page_table: Vec<PTE> = Vec::new();
+        for _i in 0..8 {
+            expected_page_table.push(PTE::default());
+        }
+
+        expected_page_table[3] = set_pte(true, 10, 15, 15, 1);
+        expected_page_table[5] = set_pte(true, 20, 2, 4, 2);
+        expected_page_table[7] = set_pte(true, 30, 1, 1, 1);
+
+        for i in 0..8 {
+            assert_eq!(page_table[i], expected_page_table[i]);
+        }
+
+        assert_eq!(page_table.len(), 8);
+        assert_eq!(frame_pool.len(), 0);
+        assert_eq!(frame, 10);
+    }
+
+    #[test]
+    pub fn test_process_page_access_lfu_page_not_in_memory_no_frame_available() {
+        let mut page_table: Vec<PTE> = Vec::new();
+        let mut frame_pool: Vec<i32> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        page_table[3] = set_pte(true, 10, 15, 15, 1);
+        page_table[5] = set_pte(true, 20, 12, 16, 2);
+        page_table[7] = set_pte(true, 30, 14, 17, 4);
+
+        let page_number: usize = 2;
+        let current_timestamp: i32 = 18;
+
+        let frame = process_page_access_lfu(
+            &mut page_table,
+            page_number,
+            &mut frame_pool,
+            current_timestamp,
+        );
+
+        let mut expected_page_table: Vec<PTE> = Vec::new();
+        for _i in 0..8 {
+            expected_page_table.push(PTE::default());
+        }
+
+        expected_page_table[2] = set_pte(true, 10, 18, 18, 1);
+        expected_page_table[5] = set_pte(true, 20, 12, 16, 2);
+        expected_page_table[7] = set_pte(true, 30, 14, 17, 4);
+
+        for i in 0..8 {
+            assert_eq!(page_table[i], expected_page_table[i]);
+        }
+
+        assert_eq!(page_table.len(), 8);
+        assert_eq!(frame_pool.len(), 0);
+        assert_eq!(frame, 10);
+    }
+
+    #[test]
+    pub fn test_count_page_faults_lfu() {
+        let mut page_table: Vec<PTE> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        let reference_string = vec![0, 3, 2, 6, 3, 4, 5, 2, 6, 4, 5];
+        let mut frame_pool: Vec<i32> = vec![0, 1, 2];
+
+        let faults = count_page_faults_lfu(&mut page_table, reference_string, &mut frame_pool);
+
+        assert_eq!(faults, 10);
+    }
+
+    #[test]
+    pub fn test_count_page_faults_lfu_case2() {
+        let mut page_table: Vec<PTE> = Vec::new();
+
+        for _i in 0..8 {
+            page_table.push(PTE::default());
+        }
+
+        page_table[2] = set_pte(true, 10, 3, 3, 1);
+        page_table[5] = set_pte(true, 20, 2, 4, 2);
+        page_table[7] = set_pte(true, 30, 1, 1, 1);
+
+        let reference_string = vec![2,5,0,2,5,2,3,5,1,2,6,0];
+        let mut frame_pool: Vec<i32> = Vec::new();
+
+        let faults = count_page_faults_lfu(&mut page_table, reference_string, &mut frame_pool);
+
+        assert_eq!(faults, 5);
+    }
+
 }
--- src/virtual/virtual.rs
+++ src/virtual/virtual.rs
@@ -86,3 +86,277 @@ pub fn process_page_access_fifo(
 
     frame
 }
+
+pub fn count_page_faults_fifo(
+    page_table: &mut Vec<PTE>,
+    page_references: Vec<i32>,
+    frame_pool: &mut Vec<i32>,
+) -> i32 {
+    let mut faults: i32 = 0;
+    let table_cnt = page_table.len();
+    let reference_count = page_references.len();
+    let mut current_timestamp = 0;
+
+    for i in 0..table_cnt {
+        if page_table[i].arrival_timestamp > current_timestamp {
+            current_timestamp = page_table[i].arrival_timestamp;
+        }
+    }
+    current_timestamp = current_timestamp + 1;
+
+    for r in 0..reference_count {
+        let page_number = page_references[r] as usize;
+
+        if page_table[page_number].is_valid == true {
+            page_table[page_number].last_access_timestamp = current_timestamp;
+            page_table[page_number].reference_count = page_table[page_number].reference_count + 1;
+            current_timestamp = current_timestamp + 1;
+        } else if frame_pool.len() > 0 {
+            let frame = match frame_pool.pop() {
+                Some(t) => t,
+                None => -1,
+            };
+            allocate_memory(page_table, page_number, frame, current_timestamp);
+            faults = faults + 1;
+            current_timestamp = current_timestamp + 1;
+        } else {
+            let mut smallest_arrival_timestamp = i32::MAX;
+            let mut position: i32 = -1;
+            for i in 0..table_cnt {
+                if page_table[i].is_valid == true
+                    && page_table[i].arrival_timestamp < smallest_arrival_timestamp
+                {
+                    smallest_arrival_timestamp = page_table[i].arrival_timestamp;
+                    position = i as i32;
+                }
+            }
+
+            if position > -1 {
+                let frame = page_table[position as usize].frame_number;
+                replace_memory(position, page_table, page_number, frame, current_timestamp);
+            }
+            faults = faults + 1;
+            current_timestamp = current_timestamp + 1;
+        }
+    }
+
+    return faults;
+}
+
+pub fn process_page_access_lru(
+    page_table: &mut Vec<PTE>,
+    page_number: usize,
+    frame_pool: &mut Vec<i32>,
+    current_timestamp: i32,
+) -> i32 {
+    let mut frame: i32 = -1;
+
+    let table_cnt = page_table.len();
+
+    if page_table[page_number].is_valid == true {
+        page_table[page_number].last_access_timestamp = current_timestamp;
+        page_table[page_number].reference_count = page_table[page_number].reference_count + 1;
+        frame = page_table[page_number].frame_number;
+    } else if frame_pool.len() > 0 {
+        frame = match frame_pool.pop() {
+            Some(t) => t,
+            None => -1,
+        };
+        allocate_memory(page_table, page_number, frame, current_timestamp);
+    } else {
+        let mut smallest_last_access_timestamp = i32::MAX;
+        let mut position: i32 = -1;
+        for i in 0..table_cnt {
+            if page_table[i].is_valid == true
+                && page_table[i].last_access_timestamp < smallest_last_access_timestamp
+            {
+                smallest_last_access_timestamp = page_table[i].last_access_timestamp;
+                position = i as i32;
+            }
+        }
+
+        if position > -1 {
+            frame = page_table[position as usize].frame_number;
+            replace_memory(position, page_table, page_number, frame, current_timestamp);
+        }
+    }
+
+    frame
+}
+
+pub fn count_page_faults_lru(
+    page_table: &mut Vec<PTE>,
+    page_references: Vec<i32>,
+    frame_pool: &mut Vec<i32>,
+) -> i32 {
+    let mut faults: i32 = 0;
+    let table_cnt = page_table.len();
+    let reference_count = page_references.len();
+    let mut current_timestamp = 0;
+
+    for i in 0..table_cnt {
+        if page_table[i].arrival_timestamp > current_timestamp {
+            current_timestamp = page_table[i].arrival_timestamp;
+        }
+    }
+    current_timestamp = current_timestamp + 1;
+
+    for r in 0..reference_count {
+        let page_number = page_references[r] as usize;
+
+        if page_table[page_number].is_valid == true {
+            page_table[page_number].last_access_timestamp = current_timestamp;
+            page_table[page_number].reference_count = page_table[page_number].reference_count + 1;
+            current_timestamp = current_timestamp + 1;
+        } else if frame_pool.len() > 0 {
+            let frame = match frame_pool.pop() {
+                Some(t) => t,
+                None => -1,
+            };
+            allocate_memory(page_table, page_number, frame, current_timestamp);
+            faults = faults + 1;
+            current_timestamp = current_timestamp + 1;
+        } else {
+            let mut smallest_last_access_timestamp = i32::MAX;
+            let mut position: i32 = -1;
+            for i in 0..table_cnt {
+                if page_table[i].is_valid == true
+                    && page_table[i].last_access_timestamp < smallest_last_access_timestamp
+                {
+                    smallest_last_access_timestamp = page_table[i].last_access_timestamp;
+                    position = i as i32;
+                }
+            }
+
+            if position > -1 {
+                let frame = page_table[position as usize].frame_number;
+                replace_memory(position, page_table, page_number, frame, current_timestamp);
+            }
+            faults = faults + 1;
+            current_timestamp = current_timestamp + 1;
+        }
+    }
+
+    return faults;
+}
+
+pub fn process_page_access_lfu(
+    page_table: &mut Vec<PTE>,
+    page_number: usize,
+    frame_pool: &mut Vec<i32>,
+    current_timestamp: i32,
+) -> i32 {
+    let mut frame: i32 = -1;
+
+    let table_cnt = page_table.len();
+
+    if page_table[page_number].is_valid == true {
+        page_table[page_number].last_access_timestamp = current_timestamp;
+        page_table[page_number].reference_count = page_table[page_number].reference_count + 1;
+        frame = page_table[page_number].frame_number;
+    } else if frame_pool.len() > 0 {
+        frame = match frame_pool.pop() {
+            Some(t) => t,
+            None => -1,
+        };
+        allocate_memory(page_table, page_number, frame, current_timestamp);
+    } else {
+        let mut smallest_reference_count = i32::MAX;
+
+        let mut positions: Vec<usize> = Vec::new();
+        for i in 0..table_cnt {
+            if page_table[i].is_valid == true {
+                if page_table[i].reference_count < smallest_reference_count {
+                    positions.clear();
+                    positions.push(i);
+                    smallest_reference_count = page_table[i].reference_count;
+                } else if page_table[i].reference_count == smallest_reference_count {
+                    positions.push(i);
+                }
+            }
+        }
+
+        if positions.len() > 0 {
+            let mut smallest_arrival_timestamp = i32::MAX;
+            let mut position: i32 = -1;
+            for idx in positions {
+                if page_table[idx].arrival_timestamp < smallest_arrival_timestamp {
+                    smallest_arrival_timestamp = page_table[idx].arrival_timestamp;
+                    position = idx as i32;
+                }
+            }
+            frame = page_table[position as usize].frame_number;
+            replace_memory(position, page_table, page_number, frame, current_timestamp);
+        }
+    }
+
+    frame
+}
+
+pub fn count_page_faults_lfu(
+    page_table: &mut Vec<PTE>,
+    page_references: Vec<i32>,
+    frame_pool: &mut Vec<i32>,
+) -> i32 {
+    let mut faults: i32 = 0;
+    let table_cnt = page_table.len();
+    let reference_count = page_references.len();
+    let mut current_timestamp = 0;
+
+    for i in 0..table_cnt {
+        if page_table[i].arrival_timestamp > current_timestamp {
+            current_timestamp = page_table[i].arrival_timestamp;
+        }
+    }
+    current_timestamp = current_timestamp + 1;
+
+    for r in 0..reference_count {
+        let page_number = page_references[r] as usize;
+
+        if page_table[page_number].is_valid == true {
+            page_table[page_number].last_access_timestamp = current_timestamp;
+            page_table[page_number].reference_count = page_table[page_number].reference_count + 1;
+            current_timestamp = current_timestamp + 1;
+        } else if frame_pool.len() > 0 {
+            let frame = match frame_pool.pop() {
+                Some(t) => t,
+                None => -1,
+            };
+            allocate_memory(page_table, page_number, frame, current_timestamp);
+            faults = faults + 1;
+            current_timestamp = current_timestamp + 1;
+        } else {
+            let mut smallest_reference_count = i32::MAX;
+
+            let mut positions: Vec<usize> = Vec::new();
+            for i in 0..table_cnt {
+                if page_table[i].is_valid == true {
+                    if page_table[i].reference_count < smallest_reference_count {
+                        positions.clear();
+                        positions.push(i);
+                        smallest_reference_count = page_table[i].reference_count;
+                    } else if page_table[i].reference_count == smallest_reference_count {
+                        positions.push(i);
+                    }
+                }
+            }
+
+            if positions.len() > 0 {
+                let mut smallest_arrival_timestamp = i32::MAX;
+                let mut position: i32 = -1;
+                for idx in positions {
+                    if page_table[idx].arrival_timestamp < smallest_arrival_timestamp {
+                        smallest_arrival_timestamp = page_table[idx].arrival_timestamp;
+                        position = idx as i32;
+                    }
+                }
+                let frame = page_table[position as usize].frame_number;
+                replace_memory(position, page_table, page_number, frame, current_timestamp);
+            }
+            faults = faults + 1;
+            current_timestamp = current_timestamp + 1;
+        }
+    }
+
+    return faults;
+}
